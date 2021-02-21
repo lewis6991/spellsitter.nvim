8ddcf418cf686f5b8e092eac0b247d3637a7fda6
Lewis Russell <lewis6991@gmail.com>
2021-02-21 12:08:04 +0000

    Update

--- lua/spellsitter.lua
+++ lua/spellsitter.lua
@@ -1,6 +1,6 @@
 local Job = require('plenary/job')
 local query = require'vim.treesitter.query'
-local ts = require'vim.treesitter'
+local get_parser = require'nvim-treesitter.parsers'.get_parser
 
 local api = vim.api
 
@@ -9,25 +9,27 @@ local M = {}
 local ns = api.nvim_create_namespace('spellsitter')
 local hl_query
 local hl = api.nvim_get_hl_id_by_name('Error')
-local changedtick = 0
+local cache = {}
+local active_bufs = {}
 
-local function del_extmarks_for_line(bufnr, lnum)
+local function del_extmarks(bufnr, lnum)
   local ms = api.nvim_buf_get_extmarks(bufnr, ns, {lnum,0}, {lnum,-1}, {})
   for _, m in ipairs(ms) do
     api.nvim_buf_del_extmark(bufnr, ns, m[1])
   end
 end
 
-local function spellcheck_line(bufnr, lnum)
-  local lang = api.nvim_buf_get_option(bufnr, "ft")
-  local parser = ts.get_parser(bufnr, lang)
-  if not parser then
-    return false
-  end
+local function add_extmark(bufnr, lnum, result)
+  api.nvim_buf_set_extmark(bufnr, ns, lnum, result.pos, {
+    end_line = lnum,
+    end_col = result.pos+#result.word,
+    hl_group = hl,
+    ephemeral = false,
+  })
+end
 
-  if not hl_query then
-    hl_query = query.get_query(parser:lang(), "highlights")
-  end
+local function spellcheck_line(bufnr, lnum)
+  local parser = get_parser(bufnr)
 
   local spellcheck = false
   parser:for_each_tree(function(tstree, _)
@@ -39,7 +41,7 @@ local function spellcheck_line(bufnr, lnum)
       return
     end
 
-    local stateiter = hl_query:iter_captures(root_node, bufnr, lnum, root_end_row )
+    local stateiter = hl_query:iter_captures(root_node, bufnr, lnum, root_end_row)
     local capture_id, _ = stateiter()
     local capture = hl_query.captures[capture_id]
     spellcheck = capture == 'comment'
@@ -48,59 +50,111 @@ local function spellcheck_line(bufnr, lnum)
   return spellcheck
 end
 
+local function process_output_line(line)
+  local parts = vim.split(line, '%s+')
+  local op = parts[1]
+  if op ~= '&' and op ~= '#' then
+    return
+  end
+  local word = parts[2]
+  local pos
+  if op == '&' then
+    pos = tonumber(parts[4]:sub(1, -2))
+  elseif op == '#' then
+    pos = tonumber(parts[3])
+  end
+  return {
+    word = word,
+    pos  = pos
+  }
+end
+
 local function on_line(_, _, bufnr, lnum)
   if not spellcheck_line(bufnr, lnum) then
     return
   end
 
-  del_extmarks_for_line(bufnr, lnum)
+  del_extmarks(bufnr, lnum)
+
+  local bcache = cache[bufnr]
+
+  if bcache[lnum] then
+    for _, r in ipairs(bcache[lnum]) do
+      add_extmark(bufnr, lnum, r)
+    end
+    return
+  end
 
   local l = api.nvim_buf_get_lines(bufnr, lnum, lnum+1, true)[1]
-
-  local results = {}
   Job:new {
     command = 'hunspell',
     writer = {l},
     on_stdout = function(_, line)
-      local parts = vim.split(line, '%s+')
-      local op = parts[1]
-      if op ~= '&' and op ~= '#' then
+      local r = process_output_line(line)
+      if not bcache[lnum] then
+        bcache[lnum] = {}
+      end
+      table.insert(bcache[lnum], r)
+      if not r then
         return
       end
-      local word = parts[2]
-      local pos
-      if op == '&' then
-        pos = tonumber(parts[4]:sub(1, -2))
-      elseif op == '#' then
-        pos = tonumber(parts[3])
-      end
-      table.insert(results, {
-        word = word,
-        pos = pos
-      })
       vim.schedule(function()
-        api.nvim_buf_set_extmark(bufnr, ns, lnum, pos, {
-          end_line = lnum,
-          end_col = pos+#word,
-          hl_group = hl,
-          ephemeral = false,
-        })
+        add_extmark(bufnr, lnum, r)
       end)
     end,
   }:start()
+end
 
+local function invalidate_cache_lines(bufnr, first, last)
+  local bcache = cache[bufnr]
+  if not bcache then
+    return
+  end
+  for i = first, last do
+    bcache[i] = nil
+  end
+end
+
+local function attach(cbuf)
+  if active_bufs[cbuf] then
+    -- Already attached
+    return
+  end
+  active_bufs[cbuf] = true
+
+  api.nvim_buf_attach(cbuf, false, {
+    on_lines = function(_, bufnr, _, first, last)
+      invalidate_cache_lines(bufnr, first, last-1)
+    end,
+    on_detach = function(_, bufnr)
+      active_bufs[bufnr] = nil
+    end
+  })
+end
+
+local function on_win(_, _, bufnr)
+  local parser = get_parser(bufnr)
+  if not parser then
+    return false
+  end
+
+  attach(bufnr)
+
+  if not hl_query then
+    hl_query = query.get_query(parser:lang(), "highlights")
+  end
+
+  if not cache[bufnr] then
+    cache[bufnr] = {}
+  end
 end
 
 function M.setup()
   api.nvim_set_decoration_provider(ns, {
-    on_win = function(_, _, bufnr)
-      local changedtick0 = api.nvim_buf_get_var(bufnr, 'changedtick')
-      if changedtick == changedtick0 then
-        return false
-      end
-      changedtick = changedtick0
-      return true
-    end;
+    on_start = function(_, tick)
+      print('TICK: '..tick)
+    end,
+    on_win = on_win,
     on_line = on_line;
   })
 end
