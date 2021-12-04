5239679caeef4bdc062d6b66ef2349a3eb621fb5
Jess Archer <jess@jessarcher.com>
2021-12-05 00:37:39 +1000

    Fix errors calling get_parser() in other buffers

--- lua/spellsitter.lua
+++ lua/spellsitter.lua
@@ -120,7 +120,13 @@ local function on_line(_, winid, bufnr, lnum)
   marks[bufnr] = marks[bufnr] or {}
   marks[bufnr][lnum+1] = nil
 
-  get_parser():for_each_tree(function(tstree, langtree)
+  local success, parser = pcall(get_parser)
+
+  if not success then
+      return
+  end
+
+  parser:for_each_tree(function(tstree, langtree)
     local root_node = tstree:root()
     local spell_query = get_query(langtree:lang())
     if spell_query then
